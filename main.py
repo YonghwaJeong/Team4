89313b7e388c45e5c99afbcc420b82a478314b1e
JeongYonghwa <yonghwa814@naver.com>
2023-12-30 11:38:36 +0900

    fix:cart

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 def main():
 	return 0
+
+cart_number = [1, 2, 3, 4]
+def cart(like, cart_num):
+    if like == False:
+        if cart_num == 1:
+            print("cart")
+
+        def hello():
+            if like == True:
+                if cart_num == True:
+                    cart_number[0] -= 1
+                    print(f'hohoho')
+                else:
+                    cart_number[1] += 1
+            else:
+                hate = hate()
+                if hate == True:
+                    return
+    return 4
